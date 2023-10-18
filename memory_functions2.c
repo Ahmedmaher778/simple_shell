69637a9a1a633c81c0848a4b918aba9cb5a3839b
Mahmoud Korim Kandel <mahmoudkandel980@gmail.com>
2023-10-18 22:58:13 +0300

    Update memory_functions2.c

--- memory_functions2.c
+++ memory_functions2.c
@@ -1,14 +1,14 @@
 #include "shell.h"
 
 /**
- * bfree - frees a pointer and NULLs the address
- * @ptr: address of the pointer to free
+ * bfree - frees all pointer and nulls address
+ * @ptr: address
  *
- * Return: 1 if freed, otherwise 0.
+ * Return: on freed 1, otherwise 0
  */
 int bfree(void **ptr)
 {
-	if (ptr && *ptr)
+	if (ptr != NULL && *ptr != NULL)
 	{
 		free(*ptr);
 		*ptr = NULL;
