bc677e93738c2b36851c72dc7a0e4fea6b348ee5
Ahmedmaher778 <ahmedmaher7200@gmail.com>
2023-10-18 12:08:15 -0700

    f

--- err_string_functions.c
+++ err_string_functions.c
@@ -2,84 +2,85 @@
 
 /**
  *_eputs - prints an input string
- * @str: the string to be printed
- *
+ * @str: string to be printed
  * Return: Nothing
- */
+*/
+
 void _eputs(char *str)
 {
-	int i = 0;
+	int l = 0;
 
 	if (!str)
 		return;
-	while (str[i] != '\0')
+	while (str[l] != '\0')
 	{
-		_eputchar(str[i]);
-		i++;
+		_eputchar(str[l]);
+		l++;
 	}
 }
 
 /**
  * _eputchar - writes the character c to stderr
- * @c: The character to print
- *
+ * @c: character to print
  * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
+ * On error, -1 is returned, and errno is set appropriately
+*/
+
 int _eputchar(char c)
 {
-	static int i;
+	static int l;
 	static char buf[WRITE_BUF_SIZE];
 
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	if (c == BUF_FLUSH || l >= WRITE_BUF_SIZE)
 	{
-		write(2, buf, i);
-		i = 0;
+		write(2, buf, l);
+		l = 0;
 	}
 	if (c != BUF_FLUSH)
-		buf[i++] = c;
+		buf[l++] = c;
 	return (1);
 }
 
 /**
  * _putfd - writes the character c to given fd
- * @c: The character to print
- * @fd: The filedescriptor to write to
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
+ * @c: character to print
+ * @fd: filedescriptor to write to
+ * Return: On success 1
+ * On error, -1 is returned, and errno is set appropriately
+*/
+
 int _putfd(char c, int fd)
 {
-	static int i;
-	static char buf[WRITE_BUF_SIZE];
+	static int l;
+	static char buff[WRITE_BUF_SIZE];
 
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	if (c == BUF_FLUSH || l >= WRITE_BUF_SIZE)
 	{
-		write(fd, buf, i);
-		i = 0;
+		write(fd, buff, l);
+		l = 0;
 	}
 	if (c != BUF_FLUSH)
-		buf[i++] = c;
+		buff[l++] = c;
 	return (1);
 }
 
 /**
  *_putsfd - prints an input string
- * @str: the string to be printed
- * @fd: the filedescriptor to write to
- *
- * Return: the number of chars put
- */
+ * @str: string to be printed
+ * @fd: filedescriptor to write to
+ * Return: number of chars put
+*/
+
 int _putsfd(char *str, int fd)
 {
-	int i = 0;
+	int l = 0;
 
 	if (!str)
 		return (0);
 	while (*str)
 	{
-		i += _putfd(*str++, fd);
+		l += _putfd(*str++, fd);
 	}
-	return (i);
+	return (l);
 }
+
