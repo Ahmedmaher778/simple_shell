1181fa56381226678d2375a56ed87b14824e7aaf
Ahmedmaher778 <ahmedmaher7200@gmail.com>
2023-10-18 10:46:01 -0700

    s

--- more_functions.c
+++ more_functions.c
@@ -3,9 +3,9 @@
 /**
  * interactive - returns true if shell is interactive mode
  * @info: struct address
- *
  * Return: 1 if interactive mode, 0 otherwise
- */
+*/
+
 int interactive(info_t *info)
 {
 	return (isatty(STDIN_FILENO) && info->readfd <= 2);
@@ -13,10 +13,11 @@ int interactive(info_t *info)
 
 /**
  * is_delim - checks if character is a delimeter
- * @c: the char to check
- * @delim: the delimeter string
+ * @c: char to check
+ * @delim: delimeter string
  * Return: 1 if true, 0 if false
- */
+*/
+
 int is_delim(char c, char *delim)
 {
 	while (*delim)
@@ -27,9 +28,9 @@ int is_delim(char c, char *delim)
 
 /**
  *_isalpha - checks for alphabetic character
- *@c: The character to input
+ *@c: character to input.
  *Return: 1 if c is alphabetic, 0 otherwise
- */
+*/
 
 int _isalpha(int c)
 {
@@ -41,34 +42,35 @@ int _isalpha(int c)
 
 /**
  *_atoi - converts a string to an integer
- *@s: the string to be converted
+ *@s: string to be converted
  *Return: 0 if no numbers in string, converted number otherwise
- */
+*/
 
 int _atoi(char *s)
 {
-	int i, sign = 1, flag = 0, output;
-	unsigned int result = 0;
+	int l, sgn = 1, flagg = 0, output;
+	unsigned int resut = 0;
 
-	for (i = 0;  s[i] != '\0' && flag != 2; i++)
+	for (l = 0;  s[l] != '\0' && flagg != 2; l++)
 	{
-		if (s[i] == '-')
-			sign *= -1;
+		if (s[l] == '-')
+			sgn *= -1;
 
-		if (s[i] >= '0' && s[i] <= '9')
+		if (s[l] >= '0' && s[l] <= '9')
 		{
-			flag = 1;
-			result *= 10;
-			result += (s[i] - '0');
+			flagg = 1;
+			resut *= 10;
+			resut += (s[l] - '0');
 		}
-		else if (flag == 1)
-			flag = 2;
+		else if (flagg == 1)
+			flagg = 2;
 	}
 
-	if (sign == -1)
-		output = -result;
+	if (sgn == -1)
+		output = -resut;
 	else
-		output = result;
+		output = resut;
 
 	return (output);
 }
+
